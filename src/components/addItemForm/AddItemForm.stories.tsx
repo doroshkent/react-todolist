80e2be212a2831df6f81b7973f1e32a7b6d8f902
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-09 19:20:50 +0100

    change description

--- src/components/addItemForm/AddItemForm.stories.tsx
+++ src/components/addItemForm/AddItemForm.stories.tsx
@@ -1,19 +1,19 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import {action} from '@storybook/addon-actions'
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
 import { AddItemForm } from "components/addItemForm/AddItemForm";
 
 const meta: Meta<typeof AddItemForm> = {
   title: 'TODOLISTS/AddItemForm',
   component: AddItemForm,
   // This component will have an automatically generated Autodocs entry
-  tags: ['autodocs'],
+  tags: [ 'autodocs' ],
   argTypes: {
     addItem: {
       description: 'Button clicked inside form',
       action: 'clicked'
     },
     item: {
-      description: 'item\'s title'
+      description: 'Title of item, that\'s being added (for label).'
     }
   },
 };
@@ -23,7 +23,7 @@ type Story = StoryObj<typeof AddItemForm>;
 
 export const AddItemFormStory: Story = {
   args: {
-    addItem: action('Button clicked inside form'),
+    addItem: action( 'Button clicked inside form' ),
     item: 'task'
   },
 };
