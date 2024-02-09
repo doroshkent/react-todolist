87f5949b85fcac3f021a1cc1a4ddbedacc385964
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-09 19:16:16 +0100

    create stories for EditItemField

---
+++ src/components/editItemField/EditItemField.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
+import { EditItemField } from "components/editItemField/EditItemField";
+
+const meta: Meta<typeof EditItemField> = {
+  title: 'TODOLISTS/EditItemField',
+  component: EditItemField,
+  tags: [ 'autodocs' ],
+  argTypes: {
+    title: {
+      description: 'Start title is empty. Add title, push Enter, set string.'
+    },
+    renameItem: {
+      description: 'Changes title. Sets error if title is empty.'
+    },
+    toggleEditMode: {
+      description: 'Hides component.'
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof EditItemField>;
+
+export const EditItemFieldStory: Story = {
+  args: {
+    title: '',
+    renameItem: action( 'title changed' ),
+    toggleEditMode: action( 'field hide' )
+  }
+};
