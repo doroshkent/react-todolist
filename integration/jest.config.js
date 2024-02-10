da066e7b1147d4715af1c396167491c7b95e9e66
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 19:28:18 +0100

    create integration test for Todolists component

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -4,14 +4,10 @@ module.exports = {
   setupFilesAfterEnv: ['./setupTests.js']
 };
 
-// todolist
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-todolist--todolist-story&args=
 // todolists
 // http://localhost:9009/iframe.html?viewMode=story&id=todolists-todolists--todolists-story&args=
 // app
 // http://localhost:9009/iframe.html?viewMode=story&id=app--app-story&args=
-//add item
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-additemform--add-item-form-story&args=
 // edit item
 // http://localhost:9009/iframe.html?viewMode=story&id=todolists-edititemfield--edit-item-field-story&args=
 // tasks
