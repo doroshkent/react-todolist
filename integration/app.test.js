bdaee699edc08c1813e74e607fef35352d15505e
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 19:33:41 +0100

    create integration test for App component

---
+++ integration/app.test.js
@@ -0,0 +1,12 @@
+describe('todolist', () => {
+  it('base example, visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto('http://localhost:9009/iframe.html?viewMode=story&id=app--app-story&args=',
+      {waitUntil: "networkidle2"});
+
+    const image = await page.screenshot();
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
+  });
+});
