21c0c94213e7df2cc13745918e53cb72417a8464
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 18:55:37 +0100

    fix Todolist story

--- src/components/todolists/todolist/Todolist.stories.tsx
+++ src/components/todolists/todolist/Todolist.stories.tsx
@@ -2,30 +2,26 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ReduxStoreProviderDecorator } from "stories/decorators/ReduxStoreProviderDecorator";
 import { ThemeProviderDecorator } from "stories/decorators/ThemeProviderDecorator";
 import { Todolist } from "components/todolists/todolist/Todolist";
+import { useSelector } from "react-redux";
+import { AppRootStateType } from "state/store";
+import { TodoListStateType } from "state/todolistsReducer";
 
 const meta: Meta<typeof Todolist> = {
-  title: 'TODOLISTS/todolists',
+  title: 'TODOLISTS/Todolist',
   component: Todolist,
   tags: [ 'autodocs' ],
   decorators: [ ReduxStoreProviderDecorator, ThemeProviderDecorator ],
-  argTypes: {
-    id: {
-      description: 'Id of displayed todolist'
-    },
-    title: {
-      description: 'Todolist\'s title'
-    },
-    filter: {
-      description: 'Determines which tasks should be shown'
-    }
-  }
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Todolist>;
 
+const Component = () => {
+  const todolists = useSelector<AppRootStateType, TodoListStateType>( state => state.todolists );
+  return <Todolist id={ todolists[0].id } filter={ todolists[0].filter } title={ todolists[0].title } />
+}
+
 export const TodolistStory: Story = {
-  args: {
-    id: "todolist"
-  }
+  render: () => <Component />
 };
