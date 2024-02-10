b965041b167d635dbec81fd98bc010859205963b
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 20:05:51 +0100

    naming fix

--- integration/app.test.js
+++ integration/app.test.js
@@ -1,4 +1,4 @@
-describe('todolist', () => {
+describe('app', () => {
   it('base example, visually looks correct', async () => {
     // APIs from jest-puppeteer
     await page.goto('http://localhost:9009/iframe.html?viewMode=story&id=app--app-story&args=',
