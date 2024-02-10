f512e321762307e66611d4062da86afeaf5a9634
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 18:20:11 +0100

    create Todolists story

---
+++ src/components/todolists/Todolists.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ReduxStoreProviderDecorator } from "stories/decorators/ReduxStoreProviderDecorator";
+import { Todolists } from "components/todolists/Todolists";
+import { ThemeProviderDecorator } from "stories/decorators/ThemeProviderDecorator";
+
+const meta: Meta<typeof Todolists> = {
+  title: 'TODOLISTS/todolists',
+  component: Todolists,
+  tags: [ 'autodocs' ],
+  decorators: [ ReduxStoreProviderDecorator, ThemeProviderDecorator ],
+};
+
+export default meta;
+type Story = StoryObj<typeof Todolists>;
+
+export const TodolistsStory: Story = {};
