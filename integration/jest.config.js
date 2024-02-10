c2767adeb57b6955ada52dd67bc9e94a870c3495
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-10 20:18:32 +0100

    create integration test for Task component

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -3,14 +3,3 @@ module.exports = {
   testRegex: './*.test.js$',
   setupFilesAfterEnv: ['./setupTests.js']
 };
-
-// todolists
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-todolists--todolists-story&args=
-// app
-// http://localhost:9009/iframe.html?viewMode=story&id=app--app-story&args=
-// edit item
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-edititemfield--edit-item-field-story&args=
-// tasks
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-tasks--tasks-story&args=
-// task
-// http://localhost:9009/iframe.html?viewMode=story&id=todolists-task--task-story&args=
