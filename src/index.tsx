f19b3c8034825eae18a5b5bce9808c869290c434
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-27 15:51:00 +0100

    Remove React.StrictMode from app rendering.

--- src/index.tsx
+++ src/index.tsx
@@ -10,17 +10,15 @@ import { Provider } from "react-redux";
 import { store } from "state/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById( "root" ) as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store} >
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={ store }>
+    <ThemeProvider theme={ theme }>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
