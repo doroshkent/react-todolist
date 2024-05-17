bb34d10a5070e40611354f9f44985c7074431c66
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-05-17 20:57:54 +0200

    Update yarn.lock with updated dependencies
    Updated the yarn.lock file to reflect the latest versions and correct paths for the included dependencies. This includes updates to "@babel/core", "@babel/helper-create-class-features-plugin", "@babel/helper-module-imports", and others ensuring that the project uses the most recent and stable plugins available.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import circleDependency from 'vite-plugin-circular-dependency'
+import path from 'node:path'
 // https://vitejs.dev/config/
-const path = require('path')
 export default defineConfig({
-  plugins: [react(), circleDependency()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
@@ -12,7 +11,18 @@ export default defineConfig({
     environment: 'jsdom',
   },
   root: './',
-  build: { outDir: 'build', emptyOutDir: true },
+  build: {
+    chunkSizeWarningLimit: 2000, // Increase this value if necessary
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
